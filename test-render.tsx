da845d10cc9a46a87c155918398dba7aa765f4ac
skyth-engine <skythengine@gmail.com>
2026-06-13 06:10:24 +0000

    fix: update test-render.tsx to use createCliRenderer + createRoot API

--- test-render.tsx
+++ test-render.tsx
@@ -1,12 +1,18 @@
-import { render } from "@opentui/react";
+import { createCliRenderer } from "@opentui/core";
+import { createRoot } from "@opentui/react";
 import React from "react";
 import fs from "fs";
 import { Markdown } from "./src/index.js";
 
 const md = fs.readFileSync("./assets/markdown-test.md", "utf-8");
 
-render(
-  <box flexDirection="column">
+const renderer = await createCliRenderer();
+createRoot(renderer).render(
+  <scrollbox
+    width={process.stdout.columns}
+    height={process.stdout.rows}
+    scrollbarY={true}
+  >
     <Markdown content={md} codeOptions={{ lineNumbers: true }} />
-  </box>
+  </scrollbox>
 );
